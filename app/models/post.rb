bb17f9fe0f2187a5d0d2938b7344685d7b2a54c9
Charbel Maroun <charbel-maroun@hotmail.com>
2023-04-28 21:50:49 -0400

    commit tree bf891ef0e52bccdf141dc2171e636545fba1b869

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)} 
+
+    validate :custom_validator
+
+    CLICKBAIT_WORDS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+
+    def custom_validator
+        if CLICKBAIT_WORDS.none?{|word| word.match title}
+            errors.add(:title, "Make more clickbaity!")
+        end
+    end
+
 end
